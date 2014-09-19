83efc3cae70eaa73babc166c6c234263a0fb71af
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-19 13:41:29 -0700

    create model, associations, migration for jobs

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ActiveRecord::Base
   belongs_to :user
+  has_many :jobs, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140919173819) do
+ActiveRecord::Schema.define(version: 20140919183456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 20140919173819) do
     t.string   "name"
     t.string   "crunchbase_path"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "jobs", force: true do |t|
+    t.string   "title"
+    t.string   "location"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "category"
+    t.text     "description"
+    t.text     "how_to_apply"
+    t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
