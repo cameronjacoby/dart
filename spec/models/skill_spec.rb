ef54c81922dc62771f54ce38b6d478a2bb6f52f7
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-20 13:38:22 -0700

    add specs for skill model

--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Skill, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject {FactoryGirl.create(:skill)}
+
+  it "should have a valid factory" do
+    expect(subject).to be_valid
+  end
+
+  it 'should validate presence of name' do
+    skill = Skill.create({})
+    expect(skill.errors.size).to eql(1)
+  end
+
+  it 'should validate uniqueness of name' do
+    name = Faker::Lorem.words(2).join(' ')
+    Skill.create({
+      :name => name
+    })
+    skill = Skill.create({
+      :name => name
+    })
+    expect(skill.errors.size).to eql(1)
+  end
+
+  it 'should have many jobs' do
+    example_job = FactoryGirl.create(:job)
+    subject.jobs << example_job
+    expect(subject.jobs.length).to eql(1)
+    expect(example_job.skills.length).to eql(1)
+  end
+
 end
