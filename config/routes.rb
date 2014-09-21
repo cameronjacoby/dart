f142d5f6ee208ef946e1193d02edd262d29447e7
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-21 01:01:51 -0700

    add route for signup; edit styling for trending skills

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :seekers, except: [:index, :new, :edit]
-
-  resources :companies, except: [:index, :new, :edit] do
-    resources :jobs, except: [:index, :new, :edit]
+  resources :companies, only: [:show] do
+    resources :jobs, only: [:show]
   end
 
   resources :jobs, only: [:index]
@@ -14,6 +12,8 @@ Rails.application.routes.draw do
   resources :company_templates, only: [:show]
   resources :job_templates, only: [:index, :show]
   resources :skill_templates, only: [:show]
+
+  get '/signup', to: 'users#new'
 
   root 'jobs#index'
   match '*path', to: 'jobs#index', via: 'get'
