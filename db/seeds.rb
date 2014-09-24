97a4df9eb8f6ee32f6ff799f97c827f76e9dccf9
cameronjacoby <cameron.jacoby@gmail.com>
2014-09-23 21:02:32 -0700

    create better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,121 +1,32 @@
-# create skills
-html = Skill.create({name: 'html'})
-css = Skill.create({name: 'css'})
-bootstrap = Skill.create({name: 'bootstrap'})
-javascript = Skill.create({name: 'javascript'})
-jquery = Skill.create({name: 'jquery'})
-ajax = Skill.create({name: 'ajax'})
-ror = Skill.create({name: 'ruby on rails'})
-postgres = Skill.create({name: 'postgres'})
-objective_c = Skill.create({name: 'objective-c'})
-swift = Skill.create({name: 'swift'})
-java = Skill.create({name: 'java'})
-python = Skill.create({name: 'python'})
-android = Skill.create({name: 'android'})
-django = Skill.create({name: 'django'})
-node = Skill.create({name: 'node'})
-redis = Skill.create({name: 'redis'})
-backbone = Skill.create({name: 'backbone'})
-d3 = Skill.create({name: 'd3'})
+companies = ["Weft", "Benzinga", "Cerego", "FanJam", "Kaggle", "Lyst", "Thumbtack", "Shortcut",
+  "Khan Academy", "Believe.in", "Typeform", "PythonAnywhere", "FoundationDB", "Ginger.io",
+  "Plethora.io", "Sencha", "VaxTrac", "Heap", "BeanGenius", "Planet Labs"]
 
+companies.each do |company_name|
+  Company.create({name: company_name})
+end
 
-# create seekers
-user_cameron = User.create({email: 'cameron.jacoby@gmail.com', password: '12341234', is_seeker?: true})
-user_cameron.seeker = Seeker.create({first_name: 'Cameron', last_name: 'Jacoby'})
-cameron = user_cameron.seeker
+cities = ["San Francisco", "Los Angeles", "New York, NY", "Chicago", "London"]
+categories = ["Full Stack", "Front End", "Back End", "Mobile - iOS", "Mobile - Android", "Other"]
 
-user_mario = User.create({email: 'mario.c.delgado@gmail.com', password: '12341234', is_seeker?: true})
-user_mario.seeker = Seeker.create({first_name: 'Mario', last_name: 'Delgado'})
-mario = user_mario.seeker
+Company.all.each do |company|
+  company.get_crunchbase_path
+  company.get_crunchbase_profile
+  5.times do 
+    company.jobs.create({
+      title: Faker::HipsterIpsum.words(2).join(' '),
+      location: cities.sample,
+      category: categories.sample,
+      description: Faker::HipsterIpsum.paragraph,
+      how_to_apply: Faker::HipsterIpsum.sentence
+    })
+  end
+end
 
+20.times do
+  Skill.create({name: Faker::HipsterIpsum.words(1).join(' ')})
+end
 
-# create company & jobs
-user_thumbtack = User.create({email: 'hello@thumbtack.com', password: '12341234', is_company?: true})
-user_thumbtack.company = Company.create({name: 'Thumbtack', crunchbase_path: 'organization/thumbtack'})
-thumbtack = user_thumbtack.company
-
-thumbtack_frontend = Job.create({
-  :title => 'Front-End Developer',
-  :location => 'San Francisco, CA',
-  :category => 'Front End',
-  :description => Faker::Lorem.words(30).join(' '),
-  :how_to_apply => Faker::Lorem.words(10).join(' ')
-})
-
-thumbtack_frontend.skills << [html, css, bootstrap, javascript, jquery, ajax]
-
-thumbtack_backend = Job.create({
-  :title => 'Back-End Developer',
-  :location => 'San Francisco, CA',
-  :category => 'Back End',
-  :description => Faker::Lorem.words(30).join(' '),
-  :how_to_apply => Faker::Lorem.words(10).join(' ')
-})
-
-thumbtack_backend.skills << [ror, postgres]
-
-thumbtack_ios = Job.create({
-  :title => 'iOS Developer',
-  :location => 'San Francisco, CA',
-  :category => 'Mobile - iOS',
-  :description => Faker::Lorem.words(30).join(' '),
-  :how_to_apply => Faker::Lorem.words(10).join(' ')
-})
-
-thumbtack_ios.skills << [objective_c, swift]
-
-thumbtack_android = Job.create({
-  :title => 'Android Developer',
-  :location => 'San Francisco, CA',
-  :category => 'Mobile - Android',
-  :description => Faker::Lorem.words(30).join(' '),
-  :how_to_apply => Faker::Lorem.words(10).join(' ')
-})
-
-thumbtack_android.skills << [java, python]
-
-# associate jobs to company
-thumbtack.jobs << [thumbtack_frontend, thumbtack_backend, thumbtack_ios, thumbtack_android]
-
-
-# create company & jobs
-user_ginger = User.create({email: 'hello@ginger.io', password: '12341234', is_company?: true})
-user_ginger.company = Company.create({name: 'Ginger.io', crunchbase_path: 'organization/ginger-io'})
-ginger = user_ginger.company
-
-ginger_android = Job.create({
-  :title => 'Android Developer',
-  :location => 'San Francisco, CA',
-  :category => 'Mobile - Android',
-  :description => Faker::Lorem.words(30).join(' '),
-  :how_to_apply => Faker::Lorem.words(10).join(' ')
-})
-
-ginger_android.skills << [android, python, django, javascript]
-
-# associate jobs to company
-ginger.jobs << [ginger_android]
-
-
-# create company & jobs
-user_heap = User.create({email: 'hello@heapanalytics.com', password: '12341234', is_company?: true})
-user_heap.company = Company.create({name: 'Heap', crunchbase_path: 'organization/heap'})
-heap = user_heap.company
-
-heap_engineer = Job.create({
-  :title => 'Software Engineer',
-  :location => 'San Francisco, CA',
-  :category => 'Full Stack',
-  :description => Faker::Lorem.words(30).join(' '),
-  :how_to_apply => Faker::Lorem.words(10).join(' ')
-})
-
-heap_engineer.skills << [node, redis, postgres, backbone, d3]
-
-# associate jobs to company
-heap.jobs << [heap_engineer]
-
-
-# associate (favorite) jobs to seekers
-cameron.jobs << [thumbtack_frontend, thumbtack_backend, heap_engineer]
-mario.jobs << [thumbtack_ios, thumbtack_android, ginger_android]
+Job.all.each do |job|
+  job.skills << Skill.all.sample(5)
+end
