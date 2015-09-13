0491b6e475881bd1f6aa02e98aa160c18f4e0a79
cameronjacoby <cameron.jacoby@gmail.com>
2015-09-12 22:57:25 -0700

    colors, fonts

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,6 +2,10 @@ var Router = angular.module('Router', []);
 
 Router.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
+    .when('/branding', {
+      templateUrl: 'branding.html',
+      controller: 'MainCtrl'
+    })
     .when('/', {
       templateUrl: 'jobs/index.html',
       controller: 'JobsIndexCtrl'
