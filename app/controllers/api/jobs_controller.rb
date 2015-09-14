26a093283a59609146d8e7343a1a050dedc849af
cameronjacoby <cameron.jacoby@gmail.com>
2015-09-14 16:50:49 -0700

    more layout

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -2,12 +2,12 @@ class Api::JobsController < ApplicationController
 
   def index
     @jobs = Job.order("created_at DESC")
-    render json: @jobs, include: [ :roles, :locations ]
+    render json: @jobs, include: [:roles, :locations]
   end
 
   def show
     @job = Job.find_by(guid: params[:id])
-    render json: @job
+    render json: @job, include: [:roles, :locations]
   end
 
 end
