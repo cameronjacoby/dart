0912a3ec946ef0ea6e48b3d0e35b3b0297253bf3
cameronjacoby <cameron.jacoby@gmail.com>
2015-10-06 18:44:38 -0700

    link to job show page

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -7,18 +7,20 @@ Filters.filter('sanitize', ['$sce', function ($sce) {
 }]);
 
 Filters.filter('sanitizeLimit', ['$sce', function ($sce) {
-  return function (htmlCode, limit) {
+  return function (htmlCode, limit, jobId) {
+    var link = '<a href="/jobs/' + jobId + '" class="normal">Learn more &raquo;</a>';
+
     if (htmlCode.length <= limit){
-      return $sce.trustAsHtml(htmlCode);
+      return $sce.trustAsHtml(htmlCode + ' ' + link);
     }
 
     if (/\s/.test(htmlCode[limit + 1]) && !(/\s|\W|\_/.test(htmlCode[limit]))){
-      return $sce.trustAsHtml(htmlCode.slice(0, limit + 1) + '&#8230;');
+      return $sce.trustAsHtml(htmlCode.slice(0, limit + 1) + '&#8230; ' + link);
     }
 
     for (var i = limit; i >= 0; i -= 1) {
       if (/\s/.test(htmlCode[i]) && !(/\s|\W|\_/.test(htmlCode[i - 1]))) {
-        return $sce.trustAsHtml(htmlCode.slice(0, i) + '&#8230;');
+        return $sce.trustAsHtml(htmlCode.slice(0, i) + '&#8230; ' + link);
       }
     }
   };
