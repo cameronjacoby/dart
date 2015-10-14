d198210763f926b0ee322b0b3448757a3507f215
cameronjacoby <cameron.jacoby@gmail.com>
2015-10-13 23:29:33 -0700

    remove branding

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -2,10 +2,6 @@ var Router = angular.module('Router', []);
 
 Router.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
   $routeProvider
-    .when('/branding', {
-      templateUrl: 'branding.html',
-      controller: 'MainCtrl'
-    })
     .when('/', {
       templateUrl: 'jobs/index.html',
       controller: 'JobsIndexCtrl'
