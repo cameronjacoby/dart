41b5d6390056502d6878866e3136ff18eeb2b07a
cameronjacoby <cameron.jacoby@gmail.com>
2015-10-14 00:14:42 -0700

    remove comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,6 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
   config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
