99cceb746d3728c3f62835e6731f598901c288dc
cameronjacoby <cameron.jacoby@gmail.com>
2015-10-14 18:03:56 -0700

    move job controller specs to api directory

--- spec/controllers/api/jobs_controller_spec.rb
+++ spec/controllers/api/jobs_controller_spec.rb
@@ -2,4 +2,56 @@ require "rails_helper"
 
 RSpec.describe Api::JobsController, type: :controller do
 
+  describe "GET #index" do
+    before do
+      FactoryGirl.create(:job)
+      @active_jobs = Job.active.order("created_at DESC")
+      @job = @active_jobs.first
+      get :index
+    end
+
+    it "should respond with 200 ok" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should assign @jobs to all active jobs" do
+      expect(assigns(:jobs)).to eq(@active_jobs)
+    end
+
+    it "should render all active jobs as json" do
+      expect(response.body).to include(@job.title.to_json)
+      expect(response.body).to include(@job.description.to_json)
+      expect(response.body).to include(@job.guid.to_json)
+    end
+
+    it "should not include job id in response" do
+      expect(response.body).not_to include(@job.id.to_json)
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      @job = FactoryGirl.create(:job)
+      get :show, id: @job.guid
+    end
+
+    it "should respond with 200 ok" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should assign @job" do
+      expect(assigns(:job)).to eq(@job)
+    end
+
+    it "should render job as json" do
+      expect(response.body).to include(@job.title.to_json)
+      expect(response.body).to include(@job.description.to_json)
+      expect(response.body).to include(@job.guid.to_json)
+    end
+
+    it "should not include job id in response" do
+      expect(response.body).not_to include(@job.id.to_json)
+    end
+  end
+
 end
